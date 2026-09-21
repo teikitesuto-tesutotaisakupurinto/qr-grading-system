2156cca911294b049d56e2591499443c79712ad9
teikitesuto-tesutotaisakupurinto <424990yamashitayui.eishinkan@gmail.com>
2026-09-21 09:27:49 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,13 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-import { observeAuth } from "@/lib/auth";
 
 export default function HomePage() {
-  const router = useRouter();
-
   useEffect(() => {
-    const unsubscribe =
-      observeAuth(
-        (user) => {
-          if (user) {
-            router.replace(
-              "/dashboard"
-            );
-          } else {
-            router.replace(
-              "/login"
-            );
-          }
-        },
-        () => {
-          router.replace(
-            "/login"
-          );
-        }
-      );
-
-    return () => {
-      unsubscribe();
-    };
-  }, [router]);
+    window.location.replace(
+      "/login"
+    );
+  }, []);
 
   return null;
 }
